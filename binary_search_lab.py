cfdea80b3eedc9573e12bbc56df5a1c4327a6915
Kajabukama <kajabukama@gmail.com>
2016-12-03 18:02:18 +0300

    Modified binary_search_lab.py

--- binary_search_lab.py
+++ binary_search_lab.py
@@ -1,6 +1,24 @@
 # a class that inherits from the list class the following
-class BinarySearch(list):
+class List:
+	a,b = 0,0
 	def __init__(self):
 		pass
 
-exa = BinarySearch()
+	def get_a(self):
+		return self.a
+
+	def set_a(self,a):
+		self.a = a
+
+	def get_b(self):
+		return self.b
+
+	def set_b(self,b):
+		self.b = b
+
+class BinarySearch(List):
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+
+
