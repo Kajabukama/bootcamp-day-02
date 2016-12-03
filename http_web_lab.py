2c3e19d55d0020cc6b6910206571786be3ea4be2
Kajabukama <kajabukama@gmail.com>
2016-12-04 01:58:57 +0300

    Modified the http_web_lab.py

--- http_web_lab.py
+++ http_web_lab.py
@@ -6,11 +6,14 @@ def http_web():
 	request = urllib.urlopen("http://google.com")
 	code = request.getcode()
 	content = request.read()
-	print(str(content))
+	print(content)
 
+# a function to read json data using urllib library
+# the function takes one argument which the API address
+# example : http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson
 
 def http_web_api(api):
-	json_data = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/{}_hour.geojson".format(api)
+	json_data = api
 	request = urllib.urlopen(json_data)
 	http_code = request.getcode()
 
@@ -20,4 +23,4 @@ def http_web_api(api):
 	else:
 		print('Sorry, we received an error : {}'.format(http_code))
 
-http_web_api(4.5)
+http_web_api("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson")
